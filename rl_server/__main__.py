72bdd982ad0dc6e93e451d3ed724446d72785f2f
Robert Fitzgerald <robfitzgerald@gmail.com>
2022-10-14 12:22:11 -0600

    main app supports driver|network servers

--- rl_server/__main__.py
+++ rl_server/__main__.py
@@ -1,3 +1,27 @@
 import rl_server.rllib_server.server_driver_policy as d_server
+import rl_server.rllib_server.server_network_policy_v2 as n_server
+import os
+import sys
+import logging
 
-d_server.run()
+log = logging.getLogger("rl_server")
+
+
+def run_main():
+    services = ['driver', 'network']
+    service = os.environ.get("SERVICE")
+
+    if service == "driver":
+        return d_server.run()
+    elif service == "network":
+        return n_server.run()
+    else:
+        log.error(f'os.environ["SERVICE"] = {service}')
+        log.error(os.environ)
+        log.error(
+            f"please set the environment variable SERVICE to one of: {str(services)}")
+        return 1
+
+
+if __name__ == "__main__":
+    sys.exit(run_main())
